0664c5b9477d91a601da0012a264fcbeecb88126
rvasquez <rvasquez1@gmail.com>
2021-01-22 18:43:34 -0500

    added the output folder validator

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# employee_summary
+# Professional README Generator
+
+## Task
+
+build a software engineering team generator command line application. The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. 
+
+When the user has completed building the team, the application will create an HTML file that displays a nicely formatted team roster based on the information provided by the user.
+
+The application will be invoked by using the following command:
+
+```bash
+node app.js
+```
+
+## User Story
+
+```
+As a manager
+I want to generate a webpage that displays my team's basic info
+so that I have quick access to emails and GitHub profiles
+```
+### Built With
+----
+```
+  * Node.js
+  * Javascript
+  * Html
+  * Test.js
+  * Node Modules
+    - inquirer (for prompt questions)
+    - validator (for some validation)
+    - jest (for tests)
+```    
+
+### How to Use
+----
+```
+  1- Type "node app.js" in the console.
+  2- Respond the questions that are going to appear in the console.
+  3- 
+  4- Watch the magic happends. Sections in the readme file created with your answers.
+  5- If you click in the table of content its going to take you to the right section.
+  6- Open the file and see your new readme.md
+```    
+### Submission
+---
+
+Github Repo URL: 
+[https://github.com/v1zconde/employee_summary](https://github.com/v1zconde/employee_summary)
+
+Deployed: 
+[https://v1zconde.github.io/employee_summary/](https://v1zconde.github.io/employee_summary/)
+
+How to use Video: 
+[Watch Video](https://drive.google.com/file/d/1n0VDey-ckMGB1k0gQ-8ltZy7i1JWiRmp/view)
+
+
+
+## Example
+---
+### Readme Generator
+![Readme Example](./assets/img/full_page.png)
+
+
+  ## Questions
+you have any questions about the repo, open an issue or contact me directly at rvasquez1@gmail.com. You can find more of my work at 
+  Github: [v1zconde](http://github.com/v1zconde)
+
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//require
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,155 +11,183 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+//global variables
 let teamGroup = [];
 let employeeId = 1;
 let newEmployee;
-function createManager(){
-        console.log("Build your superheroe Team")
-        console.log("---------------------------")
-        inquirer.prompt([{
-            type: "input",
-            name: "managerName",
-            message: "What is your manager's name?",
-            validate: (response) => {
-                if (response == "" || /\s/.test(response)){
-                    return "Please enter first or last Name";
-                };
-                return true;
-            }
+//function to create a manager
+function createManager() {
+  console.log("Build your superheroe Team");
+  console.log("---------------------------");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is your manager's name?",
+        validate: (response) => {
+          if (
+            /^[a-zA-Z]+$/.test(response) ||
+            /^[a-zA-Z]+ [a-zA-Z]+$/.test(response)
+          ) {
+            return true;
+          }
+          return "Please enter first or last Name";
         },
-        {
-            type: "input",
-            name: "managerEmail",
-            message: "What is the manager's email?",
-            validate: (response) => {
-                if (validator.isEmail(response)){
-                    return true;
-                }
-                return "Please enter valid Email";
-                
-            } 
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email?",
+        validate: (response) => {
+          if (validator.isEmail(response)) {
+            return true;
+          }
+          return "Please enter valid Email";
         },
-        {
-            type: "input",
-            name: "managerOfficeNumber",
-            message: "What is the manager office number",
-            validate: async (response) => {
-                if (isNaN(response)) {
-                    return "Please enter a Number";
-                }
-                return true;
-            }
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the manager office number",
+        validate: async (response) => {
+          if (isNaN(response)) {
+            return "Please enter a Number";
+          }
+          return true;
         },
-        {
-            type: "list",
-            name: "teamMembers",
-            message: "Do you have team members?",
-            choices: ["Yes", "No"],
-        }]).then((responses) => {
-            const manager = new Manager(responses.managerName, employeeId, responses.managerEmail, responses.managerOfficeNumber)
-            console.log(manager);
-            teamGroup.push(manager);
-            if (responses.teamMembers === "Yes") {
-                createTeam();
-            }
-            else
-            {
-                buildHtml();
-            }
-        })
-    }
-
-function createTeam(){
-    console.log("Superheroe Team")
-    inquirer.prompt([{
+      },
+      {
+        type: "list",
+        name: "teamMembers",
+        message: "Do you have team members?",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((responses) => {
+      const manager = new Manager(
+        responses.managerName,
+        employeeId,
+        responses.managerEmail,
+        responses.managerOfficeNumber
+      );
+      teamGroup.push(manager);
+      if (responses.teamMembers === "Yes") {
+        createTeam();
+      } else {
+        buildHtml();
+      }
+    });
+}
+//function to create employees (Engineer or Interns)
+function createTeam() {
+  console.log("Superheroe Team");
+  inquirer
+    .prompt([
+      {
         type: "input",
         name: "nameEmployee",
         message: "What is name Employee?",
         validate: (response) => {
-            if (response == "" || /\s/.test(response)){
-                return "Please enter the name of the employee";
-            };
+          if (
+            /^[a-zA-Z]+$/.test(response) ||
+            /^[a-zA-Z]+ [a-zA-Z]+$/.test(response)
+          ) {
             return true;
-        }
-        
-    },
-    {
+          }
+          return "Please enter the name of the employee";
+        },
+      },
+      {
         type: "input",
         name: "emailEmployee",
         message: "What is employee Email?",
         validate: (response) => {
-            if (validator.isEmail(response)){
-                return true;
-            }
-            return "Please enter valid Email";
+          if (validator.isEmail(response)) {
+            return true;
+          }
+          return "Please enter valid Email";
         },
-    },
-    {
+      },
+      {
         type: "list",
         name: "employeeRole",
         message: "What is employee Role?",
         choices: ["engineer", "intern"],
-    },
-    {
-       type: "input",
-       name: "employeeGithub",
-       message: "Whats the github of the engineer?",
-       when: (response) => {
-            return response.employeeRole == "engineer";
+      },
+      {
+        type: "input",
+        name: "employeeGithub",
+        message: "Whats the github of the engineer?",
+        when: (response) => {
+          return response.employeeRole == "engineer";
         },
         validate: (response) => {
-            if (/\s/.test(response)){
-                return "Please enter the github";
-            };
-            return true;
-        }
-    },
-    {
+          if (/^[a-zA-Z_-]+$/.test(response)) {
+            return "Please enter the github";
+          }
+          return true;
+        },
+      },
+      {
         type: "input",
         name: "employeeSchool",
         message: "Whats the school of the intern?",
         when: (response) => {
-            return response.employeeRole == "intern";
+          return response.employeeRole == "intern";
         },
         validate: (response) => {
-            if (/\s/.test(response)){
-                return "Please enter a School";
-            };
-            return true;
-        }
-    },
-    {
+          if (/^[a-zA-Z]+$/.test(response)) {
+            return "Please enter a School";
+          }
+          return true;
+        },
+      },
+      {
         type: "list",
         name: "moreEmployee",
         message: "Add more employee??",
-        choices: ["Yes", "No"]
-    }]).then((response) => {
-        employeeId++
-        if (response.employeeRole == "engineer"){
-            newEmployee = new Engineer(response.nameEmployee, employeeId, response.emailEmployee, response.employeeGithub)
-        } 
-        else
-        {
-            newEmployee = new Intern(response.nameEmployee, employeeId, response.emailEmployee, response.employeeSchool)
-        }
-        teamGroup.push(newEmployee);
-        if (response.moreEmployee === "Yes") {
-            createTeam();
-        }
-        else {
-            buildHtml();
-        }
-    })
-}
-
-function buildHtml(){
-    var page = render(teamGroup);
-    fs.writeFile("./teamPage.html", page, function (err) {
-        if (err) throw err;
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((response) => {
+      employeeId++;
+      if (response.employeeRole == "engineer") {
+        newEmployee = new Engineer(
+          response.nameEmployee,
+          employeeId,
+          response.emailEmployee,
+          response.employeeGithub
+        );
+      } else {
+        newEmployee = new Intern(
+          response.nameEmployee,
+          employeeId,
+          response.emailEmployee,
+          response.employeeSchool
+        );
+      }
+      teamGroup.push(newEmployee);
+      if (response.moreEmployee === "Yes") {
+        createTeam();
+      } else {
+        buildHtml();
+      }
     });
 }
+//function to create the html, send array employees to render the html
+function buildHtml() {
+  var page = render(teamGroup);
 
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdir(OUTPUT_DIR);
+  }
+//create the file html
+  fs.writeFile(outputPath, page, function (err) {
+    if (err) throw err;
+  });
+}
+//function init
 createManager();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
